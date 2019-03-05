afccec0bdcb7644b152826080443ec9f7fae38ae
Fabio Gubert <fgubert@Fabios-MBP.connect>
2019-03-05 22:13:09 +0000

    configured redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://fg-chat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://fg-chat.herokuapp.com', /http:\/\/fg-chat.herokuapp.com.*/ ]
 end
