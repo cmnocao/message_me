718346510a9f9a6f4f69c8b7bc513228133de83c
Fabio Gubert <fgubert@Fabios-MBP.connect>
2019-03-09 18:10:33 +0000

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
 //= require semantic-ui
-//= require_tree ./channels
 //= require_tree .
-//= require jquery
+
 
 scroll_bottom = function() {
   if ($('#messages').length > 0) {
